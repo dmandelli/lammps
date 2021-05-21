b84ecfe723ed2863ed3416a3cae7c0f61b2ab65f
Davide Mandelli <davide.mandelli86@gmail.com>
2021-05-21 17:58:38 +0200

    Fixed climber also in full OM neb

--- src/USER-MISC/fix_path_dynamics.cpp
+++ src/USER-MISC/fix_path_dynamics.cpp
@@ -1806,7 +1806,12 @@ void FixPATH_DYNAMICS::neb_force()
     else {
 
         double pref = 0.0;
-        if ( ibead == climber ) pref = -2.0*dot;
+ 	if ( ibead == climber )
+           {
+              pref = -2.0*dot;
+              dotSprTan = 0.0;
+              wgt = 0.0;
+           }
         else pref = -dot;
 
         if (PerpSpring && ( kspringPerp == 0.0 )){
